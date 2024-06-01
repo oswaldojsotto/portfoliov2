c1874047dfd749a01c1b796abc6a8f6bc86e87b2
Oswaldo <oswaldojsotto@gmail.com>
2024-06-01 04:45:55 -0400

    projectdetails

--- src/app/[locale]/components/stars-background/index.tsx
+++ src/app/[locale]/components/stars-background/index.tsx
@@ -3,7 +3,6 @@ import { useState, useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial } from "@react-three/drei";
 import { useTheme } from "next-themes";
-// import { THREE } from "three";
 import * as random from "maath/random/dist/maath-random.cjs";
 
 function Stars(props: any) {
