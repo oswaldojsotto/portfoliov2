b135e228b7a35741e59d51c3920a3ee2b181c67f
Oswaldo <oswaldojsotto@hotmail.com>
2024-07-02 23:32:22 -0400

    laptop

--- src/app/[locale]/components/copyright.tsx
+++ src/app/[locale]/components/copyright.tsx
@@ -3,10 +3,18 @@ import React from "react";
 
 const Copyright = () => {
   return (
-    <div className="-ml-[12vw]">
-      <div className="absolute flex h-16 w-72 border-2 border-dark rounded-sm"></div>
-      <div className="absolute flex h-16 w-16 border-2 border-orange rounded-sm">
-        <Image src="/logo.png" alt="logo" width={100} height={100} />
+    <div className="-ml-[12vw] ">
+      <div className="absolute flex h-8 w-56 ml-16 border-b-2 border-b-dark rounded-sm " />
+      <div className="absolute flex flex-col h-16 w-72 border-2 border-dark rounded-sm">
+        <p className="ml-16 flex  px-[14px] font-agdasima text-[20px] text-dark">
+          2024 © ALL RIGHTS RESERVED
+        </p>
+        <p className="ml-16 mt-[0.8] flex   px-[34px] font-agdasima text-[20px] text-dark">
+          OSWALDO J. SOTTO ©
+        </p>
+      </div>
+      <div className="absolute flex h-16 w-16 border-2 border-dark rounded-sm">
+        <Image src="/logo.png" alt="logo" width={70} height={70} />
       </div>
     </div>
   );
