5a418f67b1ff10a3d6022e821c336196ea10bd05
Oswaldo <oswaldojsotto@gmail.com>
2024-07-06 18:29:20 -0400

    pc

--- src/app/[locale]/components/email-button.tsx
+++ src/app/[locale]/components/email-button.tsx
@@ -1,17 +1,23 @@
-import React, { useState } from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { useTheme } from "next-themes";
 import { useClipboard } from "@/hooks/useClipboard";
 
-const EmailButton = ({ email = false }: { email: boolean }) => {
+const EmailButton = ({ email = false }: { email?: boolean }) => {
   const [copied, setCopied] = useState(false);
-  const theme = useTheme();
+  const { theme } = useTheme();
+  const [currentTheme, setCurrentTheme] = useState<string | undefined>("dark");
   const { copyToClipboard } = useClipboard();
 
-  const iconsLight = [`/mail-open-light.svg`, `/mail-light.svg`];
-  const iconsDark = [`/mail-open-dark.svg`, `/mail-dark.svg`];
+  const lightIcons = [`/mail-open-light.svg`, `/mail-light.svg`];
+  const darkIcons = [`/mail-open-dark.svg`, `/mail-dark.svg`];
 
-  const iconSet = theme.theme === "dark" ? iconsLight : iconsDark;
+  const iconSet = currentTheme === "dark" ? lightIcons : darkIcons;
+
+  useEffect(() => {
+    setCurrentTheme(theme);
+  }, [theme]);
 
   const handleClick = () => {
     copyToClipboard("oswaldojsotto@gmail.com");
@@ -25,17 +31,19 @@ const EmailButton = ({ email = false }: { email: boolean }) => {
         setCopied(true);
       }}
       onMouseLeave={() => setCopied(false)}>
-      <div className="group relative border-2 rounded-l-md w-[3rem] h-[3.1rem] border-dark dark:border-light flex justify-center ">
+      <div
+        className="group relative border-2 rounded-l-md  h-[3.1rem] border-dark dark:border-light flex 
+      justify-center z-1 w-auto ">
         <Image
-          className={copied ? "mb-[6.5px]" : ""}
+          className={`px-1  ${copied ? "mb-[6px]" : ""}`}
           src={copied ? iconSet[0] : iconSet[1]}
           alt="email"
-          width={28}
-          height={28}
+          width={36}
+          height={36}
         />
       </div>
       <div
-        className="group  w-full h-[3.1rem] overflow-hidden bg-dark  text-light dark:bg-light dark:text-dark rounded-r-md
+        className="group w-full h-[3.1rem] overflow-hidden bg-dark text-light dark:bg-light dark:text-dark rounded-r-md
       justify-end text-sm  flex flex-col transition-all duration-200  font-agdasima  cursor-pointer">
         <div className="group-hover:-translate-y-[52px] transition-all duration-200  flex justify-center flex-col  h-[3rem] ">
           <p className="text-[26px] px-2 flex justify-center pt-[4rem]  py-[1rem] font-agdasima">
