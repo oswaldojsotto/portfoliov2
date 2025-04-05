c30644022f3e9ef95f9fc097b860442ecdcb4f0a
Oswaldo <oswaldojsotto@gmail.com>
2025-04-05 14:36:19 -0400

    cv changes

--- src/app/[locale]/components/preloading/preloader.tsx
+++ src/app/[locale]/components/preloading/preloader.tsx
@@ -10,10 +10,16 @@ interface PreloaderTypes {
 
 const Preloader = ({ words }: PreloaderTypes) => {
   const [index, setIndex] = useState(0);
-  const [dimension, setDimension] = useState({ width: 0, height: 0 });
+  const [dimension, setDimension] = useState({
+    width: 0,
+    height: 0,
+  });
 
   useEffect(() => {
-    setDimension({ width: window.innerWidth, height: window.innerHeight });
+    setDimension({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
   }, []);
 
   useEffect(() => {
@@ -26,23 +32,32 @@ const Preloader = ({ words }: PreloaderTypes) => {
     );
   }, [index, words.length]);
 
-  const initialPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${
+  const initialPath = `M0 0 L${dimension.width} 0 L${
+    dimension.width
+  } ${dimension.height} Q${dimension.width / 2} ${
+    dimension.height + 300
+  } 0 ${dimension.height}  L0 0`;
+  const targetPath = `M0 0 L${dimension.width} 0 L${
+    dimension.width
+  } ${dimension.height} Q${dimension.width / 2} ${
     dimension.height
-  } Q${dimension.width / 2} ${dimension.height + 300} 0 ${
-    dimension.height
-  }  L0 0`;
-  const targetPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${
-    dimension.height
-  } Q${dimension.width / 2} ${dimension.height} 0 ${dimension.height}  L0 0`;
+  } 0 ${dimension.height}  L0 0`;
 
   const curve = {
     initial: {
       d: initialPath,
-      transition: { duration: 0.7, ease: [0.76, 0, 0.24, 1] },
+      transition: {
+        duration: 0.7,
+        ease: [0.76, 0, 0.24, 1],
+      },
     },
     exit: {
       d: targetPath,
-      transition: { duration: 0.7, ease: [0.76, 0, 0.24, 1], delay: 0.3 },
+      transition: {
+        duration: 0.7,
+        ease: [0.76, 0, 0.24, 1],
+        delay: 0.3,
+      },
     },
   };
 
